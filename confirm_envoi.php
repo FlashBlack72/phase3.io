02b01bea0045dd18aa52060dabe3873b7471f09c
FlashBlack <162977620+FlashBlack72@users.noreply.github.com>
2026-02-03 17:41:22 +0100

    Create confirm_envoi.php

---
+++ confirm_envoi.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+    <?php
+    $texte = $_POST["user_message"];
+    echo $texte
+    ?>
+
+    <br><button> OUI</button>
+    <a href="index.html"><button>NON</button></a>
+    
+  </body>
+</html>
